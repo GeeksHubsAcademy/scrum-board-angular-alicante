46f925d2355fb8cda9f48365915049f15c555377
Juan <jupegarnica@gmail.com>
2019-01-16 12:13:25 +0100

    boilerplate created with board, add-new-list, list, task, dataManagerService and who list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BoardComponent } from './board/board.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BoardComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { ShowListComponent } from './show-list/show-list.component';
+import { AddNewListComponent } from './add-new-list/add-new-list.component';
+import { ListComponent } from './list/list.component';
+import { TaskComponent } from './task/task.component';
+import { DataManagerService } from './data-manager.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, BoardComponent, ShowListComponent, AddNewListComponent, ListComponent, TaskComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [DataManagerService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
