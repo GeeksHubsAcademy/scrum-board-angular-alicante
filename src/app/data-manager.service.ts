08302fe2f667757c8096ba60480627c128c86ae4
Juan <jupegarnica@gmail.com>
2019-01-16 12:26:49 +0100

    data model created

--- src/app/data-manager.service.ts
+++ src/app/data-manager.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
 
+import { List } from './models.interface';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataManagerService {
+  data: { lists: Array<List> } = {
+    lists: [
+      {
+        listId: 0,
+        createdAt: new Date(),
+        modifiedAt: new Date(),
+        name: 'to do',
+        tasks: [
+          {
+            listId: 0,
+            taskId: 0,
+            text: 'aprender angular',
+            completed: false,
+            color: 'white',
+            createdAt: new Date(),
+            modifiedAt: new Date(),
+          },
+        ],
+      },
+    ],
+  };
 
-  constructor() { }
+  constructor() {}
 }
---
+++ src/app/models.interface.ts
@@ -0,0 +1,17 @@
+export interface Task {
+  listId: number;
+  taskId: number;
+  text: string;
+  completed: boolean;
+  color: string;
+  createdAt: Date;
+  modifiedAt: Date;
+}
+
+export interface List {
+  listId: number;
+  createdAt: Date;
+  modifiedAt: Date;
+  name: string;
+  tasks: Array<Task>;
+}
