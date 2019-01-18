12bb6b22f6d8da1b87da6d9af17269ba50b36ec3
Juan <jupegarnica@gmail.com>
2019-01-18 13:04:11 +0100

    login logic done. TODO: backend have to be refactored --> post /users/login needs to return always json!

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -5,9 +5,33 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class ApiService {
+  jwt: string;
   constructor(private http: HttpClient) {}
   register(username, password) {
     const body = { username, password };
     return this.http.post('https://apitrello.herokuapp.com/users', body).toPromise();
+  }
+  login(username, password) {
+    const body = { username, password };
+
+    return new Promise((resolve, reject) => {
+      this.http
+        .post('https://apitrello.herokuapp.com/users/login', body)
+        .toPromise()
+        .then(() => {
+          reject('User not found');
+        })
+        .catch(maybeNotAndError => {
+          if (maybeNotAndError.status === 200) {
+            const jwt = maybeNotAndError.error.text;
+            this.jwt = jwt;
+            resolve(200);
+          } else if (maybeNotAndError.status === 401) {
+            reject('Wrong password');
+          } else {
+            reject('Try again');
+          }
+        });
+    });
   }
 }
--- src/app/login-view/login-view.component.ts
+++ src/app/login-view/login-view.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-view',
   templateUrl: './login-view.component.html',
-  styleUrls: ['./login-view.component.scss']
+  styleUrls: ['./login-view.component.scss'],
 })
-export class LoginViewComponent implements OnInit {
+export class LoginViewComponent {
+  username: string;
+  password: string;
+  error: any;
+  constructor(private api: ApiService, private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  login() {
+    const { username, password } = this;
+    if (username.trim() !== '' && password.trim() !== '') {
+      this.api
+        .login(username.trim(), password.trim())
+        .then(() => {
+          this.error = undefined;
+          this.router.navigate(['/board']);
+        })
+        .catch(error => {
+          this.error = error;
+        });
+    }
   }
-
 }
